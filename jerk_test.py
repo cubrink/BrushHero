38d183c6e3442eecfbad038a54102eaf2f5cebf4
Jeffrey Phillips <sportsperson1999@yahoo.com>
2021-04-10 18:36:25 -0500

    File wasn't saved

--- jerk_test.py
+++ jerk_test.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 
 # %%
 def run_analysis(filename):
-    df = pd.read_csv(filename)
+    df_ = pd.read_csv(filename)
     fig, axes = plt.subplots(nrows=2, ncols=2)
     df_['a_mag'] = (df_['a_x']**2 + df_['a_y']**2 + df_['a_y']**2).apply(sqrt)
     df_['j_mag'] = (df_['j_x']**2 + df_['j_y']**2 + df_['j_y']**2).apply(sqrt)
@@ -13,8 +13,50 @@ def run_analysis(filename):
     df_[['a_y', 'j_y']].plot(title='Acceleration/Jerk along y-axis', ax=axes[0,1], figsize=(20,20))
     df_[['a_z', 'j_z']].plot(title='Acceleration/Jerk along z-axis', ax=axes[1,0], figsize=(20,20))
     df_[['a_mag', 'j_mag']].plot(title='Acceleration/Jerk magnitude', ax=axes[1,1], figsize=(20,20))
+    return df_
 
 # %%
-run_analysis('BackAndForthMovement.csv')
+back_forth = run_analysis('BackAndForthMovement.csv')
+
+# %%
+up_down = run_analysis('UpAndDownMovement.csv')
+# %%
+def run_comparison(file_a, file_b, jerk_only=True, transform=lambda x: x):
+    df_a = pd.read_csv(file_a).apply(transform)
+    df_b = pd.read_csv(file_b).apply(transform)
+
+    fig, axes = plt.subplots(nrows=4, ncols=2)
+    df_a['a_mag'] = (df_a['a_x']**2 + df_a['a_y']**2 + df_a['a_y']**2).apply(sqrt)
+    df_b['a_mag'] = (df_b['a_x']**2 + df_b['a_y']**2 + df_b['a_y']**2).apply(sqrt)
+    df_a['j_mag'] = (df_a['j_x']**2 + df_a['j_y']**2 + df_a['j_y']**2).apply(sqrt)
+    df_b['j_mag'] = (df_b['j_x']**2 + df_b['j_y']**2 + df_b['j_y']**2).apply(sqrt)
+
+    if jerk_only:
+        df_a[['j_x']].plot(title=f'{file_a} along x-axis', ax=axes[0,0], figsize=(20,20))
+        df_b[['j_x']].plot(title=f'{file_b} along x-axis', ax=axes[0,1], figsize=(20,20))
+        df_a[['j_y']].plot(title=f'{file_a} along y-axis', ax=axes[1,0], figsize=(20,20))
+        df_b[['j_y']].plot(title=f'{file_b} along y-axis', ax=axes[1,1], figsize=(20,20))
+        df_a[['j_z']].plot(title=f'{file_a} along z-axis', ax=axes[2,0], figsize=(20,20))
+        df_b[['j_z']].plot(title=f'{file_b} along z-axis', ax=axes[2,1], figsize=(20,20))
+        df_a[['j_mag']].plot(title=f'{file_a} magnitude', ax=axes[3,0], figsize=(20,20))
+        df_b[['j_mag']].plot(title=f'{file_b} magnitude', ax=axes[3,1], figsize=(20,20))
+    else:
+        df_a[['a_x', 'j_x']].plot(title=f'{file_a} along x-axis', ax=axes[0,0], figsize=(20,20))
+        df_b[['a_x', 'j_x']].plot(title=f'{file_b} along x-axis', ax=axes[0,1], figsize=(20,20))
+        df_a[['a_y', 'j_y']].plot(title=f'{file_a} along y-axis', ax=axes[1,0], figsize=(20,20))
+        df_b[['a_y', 'j_y']].plot(title=f'{file_b} along y-axis', ax=axes[1,1], figsize=(20,20))
+        df_a[['a_z', 'j_z']].plot(title=f'{file_a} along z-axis', ax=axes[2,0], figsize=(20,20))
+        df_b[['a_z', 'j_z']].plot(title=f'{file_b} along z-axis', ax=axes[2,1], figsize=(20,20))
+        df_a[['a_mag', 'j_mag']].plot(title=f'{file_a} magnitude', ax=axes[3,0], figsize=(20,20))
+        df_b[['a_mag', 'j_mag']].plot(title=f'{file_b} magnitude', ax=axes[3,1], figsize=(20,20))
+
+    return df_a, df_b
+# %%
+run_comparison('BackAndForthMovement.csv', 'UpAndDownMovement.csv', transform=lambda x: x**2)
+# %%
+back_forth.apply(lambda x: x**2)
+# %%
+run_comparison('BackAndForthMovement.csv', 'UpAndDownMovement.csv', transform=lambda x: x*abs(x))
+
 
 # %%
